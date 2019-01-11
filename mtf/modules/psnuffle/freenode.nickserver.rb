d8bb5655fa3212725369a8826e5b5daa655cd0ef
Gbenga Ojo <gbenga@gbengaojo.com>
2019-01-11 03:21:45 -0600

    new freenode.nickserver.rb

---
+++ mtf/modules/psnuffle/freenode.nickserver.rb
@@ -0,0 +1,10 @@
+# Psnuffle password sniffer add-on class for Freenode nickservers
+# Works with the Psnuffle sniffer auxiliary module
+#
+# Results are saved to the db when available. Incorrect credentials are sniffed
+# but marked as unsuccessful logins... (Typos are common :-) )
+#
+class SnifferFreenodeNick < BaseProtocolParser
+
+
+end
