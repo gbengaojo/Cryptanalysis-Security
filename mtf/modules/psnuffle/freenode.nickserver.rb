4c34f81aafcea951fa6e29d121b2b98fd1262058
Gbenga Ojo <gbenga@gbengaojo.com>
2019-01-16 02:11:37 -0600

    begin pattern matching cases

--- mtf/modules/psnuffle/freenode.nickserver.rb
+++ mtf/modules/psnuffle/freenode.nickserver.rb
@@ -14,15 +14,28 @@ class SnifferFreenodeNick < BaseProtocolParser
   end
 
   def parse(pkt)
-  # We want to return immediately if we do not have a packet which is not tcp
-  # or if the port is not 6667
-  return unless pkt.is_tcp?
-  return if (pkt.tcp_sport != 6667 and pkt.tcp_port != 6667
+    # We want to return immediately if we do not have a packet which is not tcp
+    # or if the port is not 6667
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 6667 and pkt.tcp_port != 6667
 
-  # Ensure that the session hash stays the same for during communication in
-  # both directions
-  s = find_session(pkt.tcp_sport == 110) ? get_session_src(pkt) : get_session_dst(pkt))
+    # Ensure that the session hash stays the same for during communication in
+    # both directions
+    s = find_session(pkt.tcp_sport == 110) ? get_session_src(pkt) : get_session_dst(pkt))
 
+    self.sigs.each_key do {k}
+      matched = nil
+      matches = nil
+    end
+
+    if (pkt.payload =~ self.sigs[k]
+      matched = k
+      matches = $1
+    end
+
+    case matched
+
+    end
   end
 
 end
