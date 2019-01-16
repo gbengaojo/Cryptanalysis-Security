187dc2de69218bed89a3018e21a76c4ee4f140c2
Gbenga Ojo <gbenga@gbengaojo.com>
2019-01-16 02:03:39 -0600

    tcp packet handling and unique session id

--- mtf/modules/psnuffle/freenode.nickserver.rb
+++ mtf/modules/psnuffle/freenode.nickserver.rb
@@ -14,6 +14,14 @@ class SnifferFreenodeNick < BaseProtocolParser
   end
 
   def parse(pkt)
+  # We want to return immediately if we do not have a packet which is not tcp
+  # or if the port is not 6667
+  return unless pkt.is_tcp?
+  return if (pkt.tcp_sport != 6667 and pkt.tcp_port != 6667
+
+  # Ensure that the session hash stays the same for during communication in
+  # both directions
+  s = find_session(pkt.tcp_sport == 110) ? get_session_src(pkt) : get_session_dst(pkt))
 
   end
 
